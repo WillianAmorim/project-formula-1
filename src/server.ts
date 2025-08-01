f6e9d566116533d8512c4bbdc789e90429a0dff4
Willian Amorim <willianamorim1@outlook.com.br>
2025-08-01 16:19:28 -0300

    adicionado rota para buscar driver por team

--- src/server.ts
+++ src/server.ts
@@ -21,53 +21,98 @@ const teams = [
 ];
 
 const drivers = [
-  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
-  { id: 2, name: "Sergio Pérez", team: "Red Bull Racing" },
-  { id: 3, name: "Charles Leclerc", team: "Ferrari" },
-  { id: 4, name: "Carlos Sainz", team: "Ferrari" }, // pode mudar após 2024 dependendo do mercado
-  { id: 5, name: "Lewis Hamilton", team: "Ferrari" }, // confirmado a ida para Ferrari em 2025
-  { id: 6, name: "George Russell", team: "Mercedes" },
-  { id: 7, name: "Lando Norris", team: "McLaren" },
-  { id: 8, name: "Oscar Piastri", team: "McLaren" },
-  { id: 9, name: "Fernando Alonso", team: "Aston Martin" },
-  { id: 10, name: "Lance Stroll", team: "Aston Martin" },
-  { id: 11, name: "Pierre Gasly", team: "Alpine" },
-  { id: 12, name: "Esteban Ocon", team: "Alpine" },
-  { id: 13, name: "Alexander Albon", team: "Williams" },
-  { id: 14, name: "Logan Sargeant", team: "Williams" },
-  { id: 15, name: "Yuki Tsunoda", team: "Visa Cash App RB" },
-  { id: 16, name: "Daniel Ricciardo", team: "Visa Cash App RB" },
-  { id: 17, name: "Valtteri Bottas", team: "Stake F1 Team Kick Sauber" },
-  { id: 18, name: "Zhou Guanyu", team: "Stake F1 Team Kick Sauber" },
-  { id: 19, name: "Kevin Magnussen", team: "Haas" },
-  { id: 20, name: "Nico Hülkenberg", team: "Haas" }
+    { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
+    { id: 2, name: "Sergio Pérez", team: "Red Bull Racing" },
+    { id: 3, name: "Charles Leclerc", team: "Ferrari" },
+    { id: 4, name: "Carlos Sainz", team: "Ferrari" }, // pode mudar após 2024 dependendo do mercado
+    { id: 5, name: "Lewis Hamilton", team: "Ferrari" }, // confirmado a ida para Ferrari em 2025
+    { id: 6, name: "George Russell", team: "Mercedes" },
+    { id: 7, name: "Lando Norris", team: "McLaren" },
+    { id: 8, name: "Oscar Piastri", team: "McLaren" },
+    { id: 9, name: "Fernando Alonso", team: "Aston Martin" },
+    { id: 10, name: "Lance Stroll", team: "Aston Martin" },
+    { id: 11, name: "Pierre Gasly", team: "Alpine" },
+    { id: 12, name: "Esteban Ocon", team: "Alpine" },
+    { id: 13, name: "Alexander Albon", team: "Williams" },
+    { id: 14, name: "Logan Sargeant", team: "Williams" },
+    { id: 15, name: "Yuki Tsunoda", team: "Visa Cash App RB" },
+    { id: 16, name: "Daniel Ricciardo", team: "Visa Cash App RB" },
+    { id: 17, name: "Valtteri Bottas", team: "Stake F1 Team Kick Sauber" },
+    { id: 18, name: "Zhou Guanyu", team: "Stake F1 Team Kick Sauber" },
+    { id: 19, name: "Kevin Magnussen", team: "Haas" },
+    { id: 20, name: "Nico Hülkenberg", team: "Haas" }
 ];
 
+
+// Busca todos os teams
 server.get('/teams', async (request, response) => {
     response.type("application/json").code(200);
     return { teams }
 })
 
+//Busca todos os drivers
 server.get("/drivers", async (request, response) => {
     response.type("application/json").code(200);
     return { drivers }
 })
 
+
+// Busca driver por ID
 interface DriveParams {
     id: string;
 }
 
-server.get<{Params: DriveParams}>("/drivers/:id", async(request, response) => {
-    const { id } = request.params
+server.get<{ Params: DriveParams }>("/drivers/:id", async (request, response) => {
+    const { id } = request.params;
     const driver = drivers.find((d) => d.id === Number(id))
 
-    if(!driver) {
+    if (!driver) {
         response.type("application/json").code(404);
         return { message: "Driver not found" }
     } else {
         response.type("application/json").code(200);
         return { driver }
     }
+})
+
+
+//Busca team por ID
+interface TeamParams {
+    id: string;
+}
+
+server.get<{ Params: TeamParams }>("/team/:id", async (request, response) => {
+    const { id } = request.params;
+    const team = teams.filter((t) => t.id === Number(id))
+
+    if (!team) {
+        response.type("application/json").code(404);
+        return { message: "Driver not found" }
+    } else {
+        response.type("application/json").code(200);
+        return { team }
+    }
+
+})
+
+//Buscar motoristas por time
+interface DriversByParams {
+    id: string;
+}
+
+server.get<{ Params: DriversByParams }>("/teams/:id/drivers", async (request, response) => {
+    const { id } = request.params;
+    const team = teams.find((t) => t.id === Number(id))
+
+    if (team) {
+        const driversByTeam = drivers.filter((d) => d.team === team.name)
+        response.type("application/json").code(200);
+        return { driversByTeam }
+    } else {
+        response.type("application/json").code(404);
+        return { message: "Driver not found" }
+    }
+
 })
 
 
